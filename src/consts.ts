e39a446bd45a0f4b273f7299cac732cc0792c235
xiucai5 <wjun58484@gmail.com>
2026-06-09 15:37:19 +0800

    init blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WG blog';
+export const SITE_DESCRIPTION = '记录 Shopify 开发、AI 工具、项目复盘和一些折腾过程。';
